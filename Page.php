4125a8dca1676ccaa5b1c2fd1abac5ca9408f375
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-09-08 18:08:40 +0200

    Update Database import path in Page.php

--- Page.php
+++ Page.php
@@ -8,7 +8,7 @@ use SplFileInfo;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Symfony\Component\Finder\Finder;
 use Thelia\Core\Template\TemplateDefinition;
-use Thelia\Install\Database;
+use Thelia\Core\Install\Database;
 use Thelia\Model\ConfigQuery;
 use Thelia\Model\Lang;
 use Thelia\Module\BaseModule;
